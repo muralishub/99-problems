2ee0d5e54932316eaf523fe92cf456027ecc256b
gonzo <gonzo@bigbird>
2019-01-09 13:19:09 +0000

    P08

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -2,7 +2,7 @@ object P08 {
   def compress[A](list: List[A]): List[A] = list match {
     case Nil                                => list
     case l :: Nil if l == list.reverse.head => list
-    case l :: ls if (l == ls.head)          => l :: compress(ls.tail)
+    case l :: ls if (l == ls.head)          => compress(ls)
     case l :: ls                            => l :: compress(ls)
   }
 }
--- src/test/scala/P08Test.scala
+++ src/test/scala/P08Test.scala
@@ -4,7 +4,10 @@ import org.scalatest.Matchers._
 
 class P08Test extends FunSpec {
   it("eliminate consecutive duplicates of list elements") {
-    compress(List(1, 1, 2, 3)) shouldBe List(1, 2, 3)
-    compress(List(1, 1, 2, 1, 3, 3)) shouldBe List(1, 2, 1, 3)
+    compress(List()) shouldBe (List())
+    compress(List(1)) shouldBe (List(1))
+    compress(List('a, 'a, 'c)) shouldBe (List('a, 'c))
+    compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) shouldBe (
+        List('a, 'b, 'c, 'a, 'd, 'e))
   }
 }
